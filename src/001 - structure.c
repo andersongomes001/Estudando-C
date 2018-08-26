7dc0c5f319f99d7ecb5c4f055a565a435c00632d
anderson <anderson@zaruc.com.br>
2018-08-26 19:58:33 -0300

    first commit

---
+++ src/001 - structure.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Estudando.c
+ Author      : Anderson Gomes
+ Version     : 1.0.1
+ Copyright   : andersongomes001
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("Ola mundo");
+	return 0;
+}
